ee6f304cd7de1b4fd83047ef9f94caaf07759653
ulle84 <u.belitz@gmx.de>
2017-04-21 20:07:59 +0200

    * better error handling

--- CMakeGui.bat
+++ CMakeGui.bat
@@ -1,23 +1,20 @@
 @echo off
 set BASE_DIR=%~dp0
 
-rem echo Checking prerequisites...
-rem 
-rem where cmake-gui >NUL 2>&1
-rem if NOT %errorlevel% EQU 0 (
-rem   echo cmake-gui not found, please install cmake and add it to the default path
-rem   pause
-rem   exit 1
-rem )
-rem echo   cmake-gui found
+set CMakeGuiPath=%BASE_DIR%..\PortablePrograms\cmake\bin\cmake-gui.exe
+
+if not exist %CMakeGuiPath% (
+  echo cmake-gui not found - please make available in:
+  echo %CMakeGuiPath%
+  pause
+  exit 1
+)
 
 set BUILD_DIR=%BASE_DIR%build
-
-echo Building Tools in %BUILD_DIR%
 
 if not exist %BUILD_DIR% (
   mkdir %BUILD_DIR%
 )
 
 cd %BUILD_DIR%
-start ..\..\PortablePrograms\cmake\bin\cmake-gui.exe %BASE_DIR%/src
+start %CMakeGuiPath% %BASE_DIR%src
