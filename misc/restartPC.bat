5a11c5b338093d376cf75bc6c4081e8c6412982e
ulle84 <u.belitz@gmx.de>
2017-04-25 19:39:54 +0200

    + update

---
+++ misc/restartPC.bat
@@ -0,0 +1,7 @@
+@echo off
+
+echo Do you really want to restart the PC?
+
+pause
+
+shutdown -r -t 0
---
+++ misc/shutdownPC.bat
@@ -0,0 +1,7 @@
+@echo off
+
+echo Do you really want to shutdown the PC?
+
+pause
+
+shutdown -s -t 0
