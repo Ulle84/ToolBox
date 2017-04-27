c3115b46734b96f1143408228df2089f4507735b
ulle84 <u.belitz@gmx.de>
2017-04-27 20:38:27 +0200

    * no comment

--- misc/restartPC.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-echo Do you really want to restart the PC?
-
-pause
-
-shutdown -r -t 0
--- misc/shutdownPC.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-echo Do you really want to shutdown the PC?
-
-pause
-
-shutdown -s -t 0
