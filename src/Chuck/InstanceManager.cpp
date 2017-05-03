72d32378fa702fb08da03587d281bc664e18d8e1
ulle84 <u.belitz@gmx.de>
2017-05-03 20:23:18 +0200

    + Chuck: InstanceManager

--- src/Chuck/InstanceManager.cpp
+++ src/Chuck/InstanceManager.cpp
@@ -1,4 +1,8 @@
 #include <QMdiArea>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QDebug>
+#include <QMetaMethod>
 
 #include "InstanceManager.h"
 
@@ -30,6 +34,35 @@ void InstanceManager::setup(const QString& fileName)
 void InstanceManager::setup(const QJsonObject& configuration, QMdiArea* mdiArea)
 {
   // TODO
+
+  QJsonArray instances = configuration["instances"].toArray();
+  QJsonArray connections = configuration["connections"].toArray();
+
+  for (auto it = instances.begin(); it != instances.end(); ++it)
+  {
+    QJsonObject instance = it->toObject();
+
+    QString className = instance["class"].toString();
+    QString id = instance["id"].toString();
+
+    QObject* object = createObject(className, id);
+
+    if (QWidget* widget = dynamic_cast<QWidget*>(object))
+      mdiArea->addSubWindow(widget);
+
+  }
+
+  for (auto it = connections.begin(); it != connections.end(); ++it)
+  {
+    QJsonObject connection = it->toObject();
+
+    QString source = connection["source"].toString();
+    QString destination = connection["destination"].toString();
+
+    connect(source, destination);
+  }
+
+
 
   //RandomNumberGenerator* randomNumberGenerator = new RandomNumberGenerator();
   //randomNumberGenerator->start();
@@ -66,7 +99,37 @@ void InstanceManager::setup(const QJsonObject& configuration, QMdiArea* mdiArea)
   //connect(map["randomNumberGenerator"], map["randomNumberGenerator"]->metaObject()->method(s), map["widget1"], map["widget1"]->metaObject()->method(d));
 }
 
+void InstanceManager::connect(const QString& source, const QString& destination)
+{
+  QStringList sourceInformation = source.split(' ');
+  QStringList destinationInformation = destination.split(' ');
 
+  if (sourceInformation.size() != 2 || destinationInformation.size() != 2)
+  {
+    throw std::exception("connect parameters are wrong");
+  }
+
+  // TODO source/destination vs. sender/receiver -> see how this is described in qt documentation!
+  if (!m_objects.contains(sourceInformation[0]))
+    throw std::exception("sender does not exist");
+
+  if (!m_objects.contains(destinationInformation[0]))
+    throw std::exception("receiver does not exist");
+
+  int sourceIndex = m_objects[sourceInformation[0]]->metaObject()->indexOfMethod(sourceInformation[1].toStdString().c_str());
+
+  if (sourceIndex < 0)
+    throw std::exception("signal does not exist");
+
+  int destinationIndex = m_objects[destinationInformation[0]]->metaObject()->indexOfMethod(destinationInformation[1].toStdString().c_str());
+
+  if (sourceIndex < 0)
+    throw std::exception("slot does not exist");
+
+  bool success = QObject::connect(m_objects[sourceInformation[0]], m_objects[sourceInformation[0]]->metaObject()->method(sourceIndex), 
+    m_objects[destinationInformation[0]], m_objects[destinationInformation[0]]->metaObject()->method(destinationIndex));
+  bool stopHere = true;
+}
 
 QObject* InstanceManager::createObject(const QString& className, const QString& id)
 {
@@ -82,7 +145,7 @@ QObject* InstanceManager::createObject(const QString& className, const QString& 
   {
     object = new RandomNumberGenerator();
   }
-  else if (className == "RandomNumberGenerator")
+  else if (className == "Widget")
   {
     object = new Widget();    
   }
@@ -92,6 +155,9 @@ QObject* InstanceManager::createObject(const QString& className, const QString& 
     // TODO define own exception type
     throw std::exception(QString("instance of class %1 could not be created").arg(className).toStdString().c_str());
   }
+
+  if (QThread* thread = dynamic_cast<QThread*>(object))
+    thread->start();
 
   m_objects[id] = object;
 
--- src/Chuck/MainWindow.cpp
+++ src/Chuck/MainWindow.cpp
@@ -11,6 +11,7 @@
 #include "ui_MainWindow.h"
 
 #include "InstanceManager.h"
+#include "Converter.h"
 
 MainWindow::MainWindow(QWidget* parent) :
   QMainWindow(parent),
@@ -26,46 +27,8 @@ MainWindow::MainWindow(QWidget* parent) :
     setGeometry(m_settings->value("geometry").toRect());
   }
 
-  QJsonObject instance;
-  QJsonArray instances;
-  
-  instance["class"] = "RandomNumberGenerator";
-  instance["id"] = "randomNumberGenerator";
-  instances.append(instance);
-
-  instance["class"] = "Widget";
-  instance["id"] = "widget0";
-  instances.append(instance);
-
-  instance["class"] = "Widget";
-  instance["id"] = "widget1";
-  instances.append(instance);
-
-  QJsonObject connection;
-  QJsonArray connections;  
-
-  connection["source"] = "randomNumberGenerator newNumberGenerated(int)";
-  connection["destination"] = "widget0 setNewGeneratedNumber(int)";
-  connections.append(connection);
-
-  connection["source"] = "randomNumberGenerator newNumberGenerated(int)";
-  connection["destination"] = "widget1 setNewGeneratedNumber(int)";
-  connections.append(connection);
-
-  connection["source"] = "widget0 parameterChanged(QString,QString)";
-  connection["destination"] = "widget1 setNewParameter(QString,QString)";
-  connections.append(connection);
-
-  connection["source"] = "widget1 parameterChanged(QString,QString)";
-  connection["destination"] = "widget0 setNewParameter(QString,QString)";
-  connections.append(connection);
-
-  QJsonObject configuration;
-  configuration["instances"] = instances;
-  configuration["connections"] = connections;  
-
   InstanceManager instanceManager;
-  instanceManager.setup(configuration, ui->mdiArea);
+  instanceManager.setup(Converter::configFileToJsonObject("Chuck", "settings.json"), ui->mdiArea);
 }
 
 MainWindow::~MainWindow()
